2dc6254ba2139e1a591bc90413324d154970a3ad
lukiafang <39850475+lukiafang@users.noreply.github.com>
2018-10-06 02:36:24 -0400

    Add files via upload

---
+++ localpro2/routes/htmlRoutes.js
@@ -0,0 +1,55 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  app.get("/regist", function(req, res) {
+    res.render("regist")
+  });
+
+  app.get("/business/signin", function(req, res) {
+    
+      res.render("signin") 
+  });
+
+  app.get("/setevents", function(req, res) {
+    res.render("setevents")
+  });
+
+  app.get("/contact", function(req, res) {
+    res.render("contactus")
+  })
+
+  app.get("/user", function(req, res) {
+    db.user.findAll({}).then(function(dbusers) {
+      res.render("user", {
+        msg: "Welcome!",
+        user: dbusers
+      });
+    });
+  });
+
+  
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
